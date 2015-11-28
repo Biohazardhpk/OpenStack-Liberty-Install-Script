77661fc3af1d26d4ce86d5115a5060f5540022bd
Biohazardhpk <biohazardcradle@yahoo.com>
2015-11-29 00:25:59 +0200

    Create 2Compute.sh

---
+++ 2Compute.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#Install Nova components
+apt-get install crudini -y
+apt-get install nova-compute sysfsutils -y
+crudini --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
+crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_host controller
+crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_userid openstack
+crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_password RABBIT_PASS
+crudini --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri http://controller:5000
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://controller:35357
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_plugin password
+crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_id default
+crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_id default
+crudini --set /etc/nova/nova.conf keystone_authtoken project_name service
+crudini --set /etc/nova/nova.conf keystone_authtoken username nova
+crudini --set /etc/nova/nova.conf keystone_authtoken password NOVA_PASS
+crudini --set /etc/nova/nova.conf DEFAULT my_ip 10.0.0.11
+crudini --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API
+crudini --set /etc/nova/nova.conf DEFAULT security_group_api neutron
+crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.NeutronLinuxBridgeInterfaceDriver
+crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
+crudini --set /etc/nova/nova.conf vnc vncserver_listen 0.0.0.0
+crudini --set /etc/nova/nova.conf vnc vncserver_proxyclient_address $my_ip
+crudini --set /etc/nova/nova.conf vnc novncproxy_base_url http://controller:6080/vnc_auto.html
+crudini --set /etc/nova/nova.conf glance host controller
+crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
+crudini --set /etc/nova/nova.conf DEFAULT verbose True
+egrep -c '(vmx|svm)' /proc/cpuinfo
+crudini --set /etc/nova/nova-compute.conf libvirt virt_type qemu
+service nova-compute restart
+rm -f /var/lib/nova/nova.sqlite
+#Install networking
+apt-get install neutron-plugin-linuxbridge-agent -y
+crudini --set /etc/neutron/neutron.conf DEFAULT rpc_backend rabbit
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_host controller
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_userid openstack
+crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_password RABBIT_PASS
+crudini --set /etc/neutron/neutron.conf DEFAULT auth_strategy keystone
+crudini --del /etc/neutron/neutron.conf keystone_authtoken
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_uri http://controller:5000
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_url http://controller:35357
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_plugin password
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_domain_id default
+crudini --set /etc/neutron/neutron.conf keystone_authtoken user_domain_id default
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_name service
+crudini --set /etc/neutron/neutron.conf keystone_authtoken username neutron
+crudini --set /etc/neutron/neutron.conf keystone_authtoken password NEUTRON_PASS
+crudini --set /etc/neutron/neutron.conf DEFAULT verbose True
+#Config linux bridege
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini linux_bridge physical_interface_mappings public:eth1
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini gre enable_gre True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini local_ip 10.0.0.11
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini l2_population True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini agent prevent_arp_spoofing True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup enable_security_group True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup firewall_driver neutron.agent.linux.iptables_firewall.IptablesFirewallDriver
+#Configure compute to use the network
+crudini --set /etc/nova/nova.conf neutron url http://controller:9696
+crudini --set /etc/nova/nova.conf neutron auth_url http://controller:35357
+crudini --set /etc/nova/nova.conf neutron auth_plugin password
+crudini --set /etc/nova/nova.conf neutron project_domain_id default
+crudini --set /etc/nova/nova.conf neutron user_domain_id default
+crudini --set /etc/nova/nova.conf neutron region_name RegionOne
+crudini --set /etc/nova/nova.conf neutron project_name service
+crudini --set /etc/nova/nova.conf neutron username neutron
+crudini --set /etc/nova/nova.conf neutron password NEUTRON_PASS
+service nova-compute restart
+service neutron-plugin-linuxbridge-agent restart
