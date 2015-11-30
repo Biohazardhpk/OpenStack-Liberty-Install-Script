2d0bdb7f737c04f153d0f31b774c79b5c4749dde
Biohazardhpk <biohazardcradle@yahoo.com>
2015-12-01 00:06:29 +0200

    Update 2Controller.sh

--- 2Controller.sh
+++ 2Controller.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#Aici trebuie relogare cel mai probabil o sa rup scriptul
+#Install the OpenStack client
 apt-get install python-openstackclient -y
 #Install SQL database
 apt-get install mariadb-server python-pymysql -y
 #Will be prompted for pass generaly use stack
+#Install crudini./
 apt-get install crudini -y
+#Create and edit the /etc/mysql/conf.d/mysqld_openstack.cnf file
 touch /etc/mysql/conf.d/mysqld_openstack.cnf
 crudini --set /etc/mysql/conf.d/mysqld_openstack.cnf mysqld bind-address 10.0.0.11
 crudini --set /etc/mysql/conf.d/mysqld_openstack.cnf mysqld default-storage-engine innodb
@@ -12,8 +14,9 @@ crudini --set /etc/mysql/conf.d/mysqld_openstack.cnf mysqld collation-server utf
 crudini --set /etc/mysql/conf.d/mysqld_openstack.cnf mysqld init-connect "'SET NAMES utf8'"
 crudini --set /etc/mysql/conf.d/mysqld_openstack.cnf mysqld character-set-server utf8
 sed -i '6 a innodb_file_per_table' /etc/mysql/conf.d/mysqld_openstack.cnf
+#Restart the database service
 service mysql restart
-# Automate Secure script install
+# Automate Secure the database service by running the mysql_secure_installation script.
 apt-get -y install expect
 SECURE_MYSQL=$(expect -c "
 set timeout 10
@@ -33,29 +36,34 @@ send \"y\r\"
 expect eof
 ")
 echo "$SECURE_MYSQL"
-aptitude -y purge expect
-#Install NoSQL database
+apt-get -y purge expect
+#Install NoSQL database Install and configure components
 apt-get install mongodb-server mongodb-clients python-pymongo -y
 sed -i 's/bind_ip = 127.0.0.1/bind_ip = 10.0.0.11/g' /etc/mongodb.conf
 sed -i '$ a smallfiles = true' /etc/mongodb.conf
+#Finalize installation
 service mongodb stop
 rm /var/lib/mongodb/journal/prealloc.*
 service mongodb start
-#Install Message Queue
+#Install Message Queue Install and configure components
 apt-get install rabbitmq-server -y
 rabbitmqctl add_user openstack RABBIT_PASS
 rabbitmqctl set_permissions openstack ".*" ".*" ".*"
 #Install the identity
+#Prerequisites
 mysql -u root --password=stack <<MYSQL_SCRIPT
 CREATE DATABASE keystone;
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'KEYSTONE_DBPASS';
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'KEYSTONE_DBPASS';
-exit;
 MYSQL_SCRIPT
+#Generate a random value to use as the administration token during initial configuration
 export ADMIN_TOKEN=$(openssl rand -hex 10)
+#Disable the keystone service from starting automatically after installation
 echo "manual" > /etc/init/keystone.override
+#Run the following command to install the packages
 apt-get install keystone apache2 libapache2-mod-wsgi \
   memcached python-memcache -y
+#Edit the /etc/keystone/keystone.conf file and complete the following actions
 crudini --set /etc/keystone/keystone.conf DEFAULT admin_token $ADMIN_TOKEN
 crudini --set /etc/keystone/keystone.conf DEFAULT verbose True
 crudini --set /etc/keystone/keystone.conf database connection mysql+pymysql://keystone:KEYSTONE_DBPASS@controller/keystone
@@ -63,17 +71,21 @@ crudini --set /etc/keystone/keystone.conf memcache servers localhost:11211
 crudini --set /etc/keystone/keystone.conf token provider uuid
 crudini --set /etc/keystone/keystone.conf token driver memcache
 crudini --set /etc/keystone/keystone.conf revoke driver sql
+#Populate the Identity service database:
 su -s /bin/sh -c "keystone-manage db_sync" keystone
-#Configure the Apache Server
+#Configure the Apache HTTP server
 sed -i '14 a ServerName controller' /etc/apache2/apache2.conf
 cp wsgi-keystone.conf /etc/apache2/sites-available/wsgi-keystone.conf
+#Enable the Identity service virtual hosts
 ln -s /etc/apache2/sites-available/wsgi-keystone.conf /etc/apache2/sites-enabled
+#Finalize the installation
 service apache2 restart
 rm -f /var/lib/keystone/keystone.db
 #Create Service Enty and API endpoints
 export OS_TOKEN=$ADMIN_TOKEN
 export OS_URL=http://controller:35357/v3
 export OS_IDENTITY_API_VERSION=3
+#Create the service entity and API endpoints
 openstack service create \
   --name keystone --description "OpenStack Identity" identity
 openstack endpoint create --region RegionOne \
@@ -98,10 +110,12 @@ openstack user create --domain default \
 openstack role create user  
 openstack role add --project demo --user demo user
 #Verify Operation
+#For security reasons, disable the temporary authentication token mechanism.Unset the temporary OS_TOKEN and OS_URL environment variables
 unset OS_TOKEN OS_URL
 sed -i 's/sizelimit url_normalize request_id build_auth_context token_auth admin_token_auth json_body ec2_extension user_crud_extension public_service/sizelimit url_normalize request_id build_auth_context token_auth json_body ec2_extension user_crud_extension public_service/g' /etc/keystone/keystone-paste.ini
 sed -i 's/sizelimit url_normalize request_id build_auth_context token_auth admin_token_auth json_body ec2_extension s3_extension crud_extension admin_service/sizelimit url_normalize request_id build_auth_context token_auth json_body ec2_extension s3_extension crud_extension admin_service/g' /etc/keystone/keystone-paste.ini
 sed -i 's/sizelimit url_normalize request_id build_auth_context token_auth admin_token_auth json_body ec2_extension_v3 s3_extension simple_cert_extension revoke_extension federation_extension oauth1_extension endpoint_filter_extension service_v3/sizelimit url_normalize request_id build_auth_context token_auth json_body ec2_extension_v3 s3_extension simple_cert_extension revoke_extension federation_extension oauth1_extension endpoint_filter_extension service_v3/g' /etc/keystone/keystone-paste.ini
+#Actually test
 openstack --os-auth-url http://controller:35357/v3 \
   --os-project-domain-id default --os-user-domain-id default \
   --os-project-name admin --os-username admin --os-password ADMIN_PASS\
@@ -112,14 +126,16 @@ openstack --os-auth-url http://controller:5000/v3 \
   token issue
 #Create Client environment Scripts the files are are presumed to be copyed and already configured
 source admin-openrc.sh
-#Add image service
+openstack token issue
+#OpenStack Image service
+#Prerequisites
 mysql -u root --password=stack <<MYSQL_SCRIPT 
 CREATE DATABASE glance;
 GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'localhost' IDENTIFIED BY 'GLANCE_DBPASS';
 GRANT ALL PRIVILEGES ON glance.* TO 'glance'@'%' IDENTIFIED BY 'GLANCE_DBPASS';
-exit;
 MYSQL_SCRIPT
 source admin-openrc.sh
+#Create the service credentials, complete these steps
 openstack user create --domain default --password GLANCE_DBPASS glance
 openstack role add --project service --user glance admin
 openstack service create --name glance \
@@ -132,7 +148,9 @@ openstack endpoint create --region RegionOne \
   image admin http://controller:9292
 #Install and configure components
 apt-get install glance python-glanceclient -y
+#Edit the /etc/glance/glance-api.conf file and complete the following actions
 crudini --set /etc/glance/glance-api.conf database connection mysql+pymysql://glance:GLANCE_DBPASS@controller/glance
+crudini --del /etc/glance/glance-api.conf keystone_authtoken
 crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_uri http://controller:5000
 crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_url http://controller:35357
 crudini --set /etc/glance/glance-api.conf keystone_authtoken auth_plugin password
@@ -146,8 +164,9 @@ crudini --set /etc/glance/glance-api.conf glance_store default_store file
 crudini --set /etc/glance/glance-api.conf glance_store filesystem_store_datadir /var/lib/glance/images/
 crudini --set /etc/glance/glance-api.conf DEFAULT notification_driver noop
 crudini --set /etc/glance/glance-api.conf DEFAULT verbose True
-#Configure glance auth
+#Edit the /etc/glance/glance-registry.conf file and complete the following actions
 crudini --set /etc/glance/glance-registry.conf database connection mysql+pymysql://glance:GLANCE_DBPASS@controller/glance
+crudini --del /etc/glance/glance-registry.conf keystone_authtoken
 crudini --set /etc/glance/glance-registry.conf keystone_authtoken auth_uri http://controller:5000
 crudini --set /etc/glance/glance-registry.conf keystone_authtoken auth_url http://controller:35357
 crudini --set /etc/glance/glance-registry.conf keystone_authtoken auth_plugin password
@@ -159,11 +178,13 @@ crudini --set /etc/glance/glance-registry.conf keystone_authtoken password GLANC
 crudini --set /etc/glance/glance-registry.conf paste_deploy flavor keystone
 crudini --set /etc/glance/glance-registry.conf DEFAULT notification_driver noop
 crudini --set /etc/glance/glance-registry.conf DEFAULT verbose True
+#Populate the Image service database
 su -s /bin/sh -c "glance-manage db_sync" glance
+#Finalize installation
 service glance-registry restart
 service glance-api restart
 rm -f /var/lib/glance/glance.sqlite
-#Verify Glance
+#Verify Glance, download an image
 echo "export OS_IMAGE_API_VERSION=2" \
   | tee -a admin-openrc.sh demo-openrc.sh
 source admin-openrc.sh
@@ -174,13 +195,14 @@ glance image-create --name "cirros" \
   --visibility public --progress
 glance image-list
 #Add the compute service
+#Prerequisites
 mysql -u root --password=stack <<MYSQL_SCRIPT 
 CREATE DATABASE nova;
 GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'localhost' IDENTIFIED BY 'NOVA_DBPASS';
 GRANT ALL PRIVILEGES ON nova.* TO 'nova'@'%' IDENTIFIED BY 'NOVA_DBPASS';
-exit;
 MYSQL_SCRIPT
 source admin-openrc.sh
+#Create the service credentials, complete these steps:
 openstack user create --domain default --password NOVA_PASS nova 
 openstack role add --project service --user nova admin
 openstack service create --name nova \
@@ -195,12 +217,14 @@ openstack endpoint create --region RegionOne \
 apt-get install nova-api nova-cert nova-conductor \
   nova-consoleauth nova-novncproxy nova-scheduler \
   python-novaclient -y
+#Edit the /etc/nova/nova.conf file and complete the following actions
 crudini --set /etc/nova/nova.conf database connection mysql+pymysql://nova:NOVA_DBPASS@controller/nova
 crudini --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_host controller
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_userid openstack
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_password RABBIT_PASS
 crudini --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
+crudini --del /etc/nova/nova.conf keystone_authtoken
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri http://controller:5000
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://controller:35357
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_plugin password
@@ -214,8 +238,8 @@ crudini --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutron
 crudini --set /etc/nova/nova.conf DEFAULT security_group_api neutron
 crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.NeutronLinuxBridgeInterfaceDriver
 crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
-crudini --set /etc/nova/nova.conf vnc vncserver_listen $my_ip
-crudini --set /etc/nova/nova.conf vnc vncserver_proxyclient_address $my_ip
+crudini --set /etc/nova/nova.conf vnc vncserver_listen 10.0.0.11
+crudini --set /etc/nova/nova.conf vnc vncserver_proxyclient_address 10.0.0.11
 crudini --set /etc/nova/nova.conf glance host controller
 crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
 crudini --set /etc/nova/nova.conf DEFAULT enabled_apis osapi_compute,metadata
@@ -233,14 +257,15 @@ rm -f /var/lib/nova/nova.sqlite
 source admin-openrc.sh
 nova service-list
 nova image-list
-#Install Neutron
+#Add the Networking service
+#Prerequisites
 mysql -u root --password=stack <<MYSQL_SCRIPT 
 CREATE DATABASE neutron;
 GRANT ALL PRIVILEGES ON neutron.* TO neutron@'localhost' IDENTIFIED BY 'NEUTRON_DBPASS';
 GRANT ALL PRIVILEGES ON neutron.* TO neutron@'%' IDENTIFIED BY 'NEUTRON_DBPASS';
-exit;
 MYSQL_SCRIPT
 source admin-openrc.sh
+#To create the service credentials, complete these steps:
 openstack user create --domain default --password NEUTRON_PASS neutron
 openstack role add --project service --user neutron admin
 openstack service create --name neutron \
@@ -251,6 +276,7 @@ openstack endpoint create --region RegionOne \
   network internal http://controller:9696
 openstack endpoint create --region RegionOne \
   network admin http://controller:9696
+#Networking Option 2: Self-service networks
 #Instal and config neutron on controller
 apt-get install neutron-server neutron-plugin-ml2 \
   neutron-plugin-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent \
@@ -287,32 +313,36 @@ crudini --set /etc/neutron/neutron.conf nova username nova
 crudini --set /etc/neutron/neutron.conf nova password NOVA_PASS
 crudini --set /etc/neutron/neutron.conf DEFAULT verbose True
 #Ml2 plugin config, layer 3 and dhcp
-crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 type_drivers flat,gre
-crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 tenant_network_types gre
-crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 mechanism_drivers linuxbridge,l2population,openvswitch
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 type_drivers flat,vlan,vxlan
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 tenant_network_types vxlan
+#The Linux bridge agent only supports VXLAN overlay networks.
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 mechanism_drivers linuxbridge,l2population
 crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 extension_drivers port_security
 crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_flat flat_networks public
-crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_gre tunnel_id_ranges 1:1000
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_vxlan vni_ranges 1:1000
 crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_ipset True
+#Configure the Linux bridge agent
 crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini linux_bridge physical_interface_mappings public:eth1
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini gre enable_gre True
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini gre local_ip 10.0.0.11
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini gre l2_population True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan enable_vxlan True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan local_ip 10.0.0.11
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan l2_population True
 crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini agent prevent_arp_spoofing True
 crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup enable_security_group True
 crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup firewall_driver neutron.agent.linux.iptables_firewall.IptablesFirewallDriver
+#Configure the layer-3 agent
 crudini --set /etc/neutron/l3_agent.ini DEFAULT interface_driver neutron.agent.linux.interface.BridgeInterfaceDriver
 crudini --set /etc/neutron/l3_agent.ini DEFAULT external_network_bridge 
 crudini --set /etc/neutron/l3_agent.ini DEFAULT verbose True
+#Configure the DHCP agent
 crudini --set /etc/neutron/dhcp_agent.ini DEFAULT interface_driver neutron.agent.linux.interface.BridgeInterfaceDriver
 crudini --set /etc/neutron/dhcp_agent.ini DEFAULT dhcp_driver neutron.agent.linux.dhcp.Dnsmasq
 crudini --set /etc/neutron/dhcp_agent.ini DEFAULT enable_isolated_metadata True
 crudini --set /etc/neutron/dhcp_agent.ini DEFAULT verbose True
 crudini --set /etc/neutron/dhcp_agent.ini DEFAULT dnsmasq_config_file /etc/neutron/dnsmasq-neutron.conf
-#config DHCP
+#Create and set DHCP
 touch /etc/neutron/dnsmasq-neutron.conf
 echo "dhcp-option-force=26,1450" >> /etc/neutron/dnsmasq-neutron.conf
-#Cobnfig metadata
+#Config metadata
 crudini --set /etc/neutron/metadata_agent.ini DEFAULT auth_uri http://controller:5000
 crudini --set /etc/neutron/metadata_agent.ini DEFAULT auth_url http://controller:35357
 crudini --set /etc/neutron/metadata_agent.ini DEFAULT auth_region RegionOne
@@ -337,9 +367,8 @@ crudini --set /etc/nova/nova.conf neutron username neutron
 crudini --set /etc/nova/nova.conf neutron password NEUTRON_PASS
 crudini --set /etc/nova/nova.conf neutron service_metadata_proxy True
 crudini --set /etc/nova/nova.conf neutron metadata_proxy_shared_secret METADATA_SECRET
-crudini --set /etc/nova/nova.conf DEFAULT service_metadata_proxy True
-crudini --set /etc/nova/nova.conf DEFAULT metadata_proxy_shared_secret METADATA_SECRET
-#populate neutron
+#Finalize installation
+#Populate neutron
 su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf \
   --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
 #Restart the Networking and Compute services.
@@ -350,9 +379,13 @@ service neutron-dhcp-agent restart
 service neutron-metadata-agent restart
 service neutron-l3-agent restart
 rm -f /var/lib/neutron/neutron.sqlite
+#Neutron verification
+source admin-openrc.sh
+neutron agent-list
+neutron ext-list
 #Dashboard installl
 apt-get install openstack-dashboard -y
-#to check if not working if sed is passing the lines!!!
+#Edit the /etc/openstack-dashboard/local_settings.py file and complete the following actions:
 sed -i 's/OPENSTACK_HOST = "127.0.0.1"/OPENSTACK_HOST = "controller"/g' /etc/openstack-dashboard/local_settings.py
 sed -i 's/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"/OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"/g' /etc/openstack-dashboard/local_settings.py
 sed -i '675d' /etc/openstack-dashboard/local_settings.py
@@ -360,21 +393,5 @@ echo "ALLOWED_HOSTS = ['*', ]" >> /etc/openstack-dashboard/local_settings.py
 service apache2 reload
 #Restart Controller+network
 source admin-openrc.sh
-service mysql restart
-service glance-registry restart
-service glance-api restart
-service nova-api restart
-service nova-cert restart
-service nova-consoleauth restart
-service nova-scheduler restart
-service nova-conductor restart
-service nova-novncproxy restart
-service neutron-server restart
-service neutron-plugin-linuxbridge-agent restart
-service neutron-dhcp-agent restart
-service neutron-metadata-agent restart
-service neutron-l3-agent restart
-service apache2 reload
 nova service-list
 neutron agent-list
-
