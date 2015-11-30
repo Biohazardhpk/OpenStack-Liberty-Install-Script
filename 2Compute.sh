1b33ffd3451132f7d2075bc1cb3a7e0f126950a5
Biohazardhpk <biohazardcradle@yahoo.com>
2015-12-01 00:06:01 +0200

    Update 2Compute.sh

--- 2Compute.sh
+++ 2Compute.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#Install Nova components
+echo "#Install Nova components"
 apt-get install crudini -y
 apt-get install nova-compute sysfsutils -y
+echo "#Edit the /etc/nova/nova.conf file and complete the following actions:"
 crudini --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_host controller
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_userid openstack
 crudini --set /etc/nova/nova.conf oslo_messaging_rabbit rabbit_password RABBIT_PASS
 crudini --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
+crudini --del /etc/nova/nova.conf keystone_authtoken
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_uri http://controller:5000
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://controller:35357
 crudini --set /etc/nova/nova.conf keystone_authtoken auth_plugin password
@@ -20,18 +22,28 @@ crudini --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutron
 crudini --set /etc/nova/nova.conf DEFAULT security_group_api neutron
 crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.NeutronLinuxBridgeInterfaceDriver
 crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
+crudini --set /etc/nova/nova.conf vnc my_ip 10.0.0.21
 crudini --set /etc/nova/nova.conf vnc vncserver_listen 0.0.0.0
-crudini --set /etc/nova/nova.conf vnc vncserver_proxyclient_address $my_ip
+crudini --set /etc/nova/nova.conf vnc vncserver_proxyclient_address 10.0.0.21
 crudini --set /etc/nova/nova.conf vnc novncproxy_base_url http://controller:6080/vnc_auto.html
 crudini --set /etc/nova/nova.conf glance host controller
 crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
 crudini --set /etc/nova/nova.conf DEFAULT verbose True
+echo "Finalize installation"
+echo "If this command returns a value of one or greater, your compute node supports hardware acceleration which typically requires no additional configuration.
+If this command returns a value of zero, 
+your compute node does not support hardware acceleration and 
+you must configure libvirt to use QEMU instead of KVM."
 egrep -c '(vmx|svm)' /proc/cpuinfo
+sleep 8
+echo "#Edit the [libvirt] section in the /etc/nova/nova-compute.conf file as follows"
 crudini --set /etc/nova/nova-compute.conf libvirt virt_type qemu
 service nova-compute restart
 rm -f /var/lib/nova/nova.sqlite
-#Install networking
+echo "#Install networking"
 apt-get install neutron-plugin-linuxbridge-agent -y
+echo "#Edit the /etc/neutron/neutron.conf file and complete the following actions:"
+echo "#In the [database] section, comment out any connection options because compute nodes do not directly access the database."
 crudini --set /etc/neutron/neutron.conf DEFAULT rpc_backend rabbit
 crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_host controller
 crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_userid openstack
@@ -47,15 +59,15 @@ crudini --set /etc/neutron/neutron.conf keystone_authtoken project_name service
 crudini --set /etc/neutron/neutron.conf keystone_authtoken username neutron
 crudini --set /etc/neutron/neutron.conf keystone_authtoken password NEUTRON_PASS
 crudini --set /etc/neutron/neutron.conf DEFAULT verbose True
-#Config linux bridege
+echo "#Config linux bridege"
 crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini linux_bridge physical_interface_mappings public:eth1
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini gre enable_gre True
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini gre local_ip 10.0.0.11
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini gre l2_population True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan enable_vxlan True
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan local_ip 10.0.0.21
+crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan l2_population True
 crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini agent prevent_arp_spoofing True
 crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup enable_security_group True
 crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup firewall_driver neutron.agent.linux.iptables_firewall.IptablesFirewallDriver
-#Configure compute to use the network
+echo "#Configure compute to use the network"
 crudini --set /etc/nova/nova.conf neutron url http://controller:9696
 crudini --set /etc/nova/nova.conf neutron auth_url http://controller:35357
 crudini --set /etc/nova/nova.conf neutron auth_plugin password
@@ -67,4 +79,3 @@ crudini --set /etc/nova/nova.conf neutron username neutron
 crudini --set /etc/nova/nova.conf neutron password NEUTRON_PASS
 service nova-compute restart
 service neutron-plugin-linuxbridge-agent restart
-
