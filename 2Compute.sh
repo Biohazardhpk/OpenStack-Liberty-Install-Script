adc9b08b492d8fa2f95688a4c4b5a55909ec7e49
Biohazardhpk <biohazardcradle@yahoo.com>
2016-01-03 12:37:41 +0200

    Update 2Compute.sh

--- 2Compute.sh
+++ 2Compute.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo "#Install Nova components"
 apt-get install crudini -y
+#Xauthority shit
+chmod 0600 ~/.Xauthority
+#Network packets forward
+sed -i 's/#net.ipv4.conf.default.rp_filter=1/net.ipv4.conf.default.rp_filter=0/g' /etc/sysctl.conf
+sed -i 's/#net.ipv4.conf.all.rp_filter=1/net.ipv4.conf.all.rp_filter=0/g' /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-arptables=1" >> /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-iptables=1" >> /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-ip6tables=1" >> /etc/sysctl.conf
+sysctl -p
+#Nova install
 apt-get install nova-compute sysfsutils -y
 echo "#Edit the /etc/nova/nova.conf file and complete the following actions:"
 crudini --set /etc/nova/nova.conf DEFAULT rpc_backend rabbit
@@ -20,7 +30,7 @@ crudini --set /etc/nova/nova.conf keystone_authtoken password NOVA_PASS
 crudini --set /etc/nova/nova.conf DEFAULT my_ip 10.0.0.21
 crudini --set /etc/nova/nova.conf DEFAULT network_api_class nova.network.neutronv2.api.API
 crudini --set /etc/nova/nova.conf DEFAULT security_group_api neutron
-crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.NeutronLinuxBridgeInterfaceDriver
+crudini --set /etc/nova/nova.conf DEFAULT linuxnet_interface_driver nova.network.linux_net.LinuxOVSInterfaceDriver
 crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
 crudini --set /etc/nova/nova.conf vnc my_ip 10.0.0.21
 crudini --set /etc/nova/nova.conf vnc vncserver_listen 0.0.0.0
@@ -41,14 +51,17 @@ crudini --set /etc/nova/nova-compute.conf libvirt virt_type qemu
 service nova-compute restart
 rm -f /var/lib/nova/nova.sqlite
 echo "#Install networking"
-apt-get install neutron-plugin-linuxbridge-agent -y
+apt-get install -y neutron-plugin-ml2 neutron-plugin-openvswitch-agent
 echo "#Edit the /etc/neutron/neutron.conf file and complete the following actions:"
 echo "#In the [database] section, comment out any connection options because compute nodes do not directly access the database."
+sed -i 's/connection =/#/g' /etc/neutron/neutron.conf
 crudini --set /etc/neutron/neutron.conf DEFAULT rpc_backend rabbit
 crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_host controller
 crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_userid openstack
 crudini --set /etc/neutron/neutron.conf oslo_messaging_rabbit rabbit_password RABBIT_PASS
 crudini --set /etc/neutron/neutron.conf DEFAULT auth_strategy keystone
+crudini --set /etc/neutron/neutron.conf DEFAULT service_plugins router
+crudini --set /etc/neutron/neutron.conf DEFAULT allow_overlapping_ips True
 crudini --del /etc/neutron/neutron.conf keystone_authtoken
 crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_uri http://controller:5000
 crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_url http://controller:35357
@@ -59,14 +72,17 @@ crudini --set /etc/neutron/neutron.conf keystone_authtoken project_name service
 crudini --set /etc/neutron/neutron.conf keystone_authtoken username neutron
 crudini --set /etc/neutron/neutron.conf keystone_authtoken password NEUTRON_PASS
 crudini --set /etc/neutron/neutron.conf DEFAULT verbose True
-echo "#Config linux bridege"
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini linux_bridge physical_interface_mappings public:eth1
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan enable_vxlan True
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan local_ip 10.0.0.21
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini vxlan l2_population True
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini agent prevent_arp_spoofing True
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup enable_security_group True
-crudini --set /etc/neutron/plugins/ml2/linuxbridge_agent.ini securitygroup firewall_driver neutron.agent.linux.iptables_firewall.IptablesFirewallDriver
+echo "#Config the OVS"
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 type_drivers flat,gre
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 tenant_network_types gre
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2 mechanism_drivers openvswitch
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ml2_type_gre tunnel_id_ranges 1:1000
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_security_group True
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup enable_ipset True
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini securitygroup firewall_driver neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs local_ip 10.0.1.21
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini ovs enable_tunneling True
+crudini --set /etc/neutron/plugins/ml2/ml2_conf.ini agent tunnel_types gre
 echo "#Configure compute to use the network"
 crudini --set /etc/nova/nova.conf neutron url http://controller:9696
 crudini --set /etc/nova/nova.conf neutron auth_url http://controller:35357
@@ -77,5 +93,6 @@ crudini --set /etc/nova/nova.conf neutron region_name RegionOne
 crudini --set /etc/nova/nova.conf neutron project_name service
 crudini --set /etc/nova/nova.conf neutron username neutron
 crudini --set /etc/nova/nova.conf neutron password NEUTRON_PASS
+service openvswitch-switch restart
 service nova-compute restart
-service neutron-plugin-linuxbridge-agent restart
+service neutron-plugin-openvswitch-agent restart
